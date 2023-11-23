f8c2f6b3c46bac885b12a7323d6ca1360d46d694
Anton Gridin <titokila17@gmail.com>
2023-11-23 17:09:15 +0300

    Вступительное задание ver1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,52 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-
-    void clear() {
-    }
-
-    void save(Resume r) {
-    }
-
-    Resume get(String uuid) {
-        return null;
-    }
-
-    void delete(String uuid) {
-    }
+    private static final int CAPACITY = 10000;
+    private final Resume[] storage = new Resume[CAPACITY];
+    private int countResume;
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, countResume);
     }
 
-    int size() {
-        return 0;
+    public int size() {
+        return countResume;
+    }
+
+    public void save(Resume r) {
+        if (countResume < CAPACITY) {
+            storage[countResume++] = r;
+        }
+    }
+
+    public void delete(String uuid) {
+        for (int i = 0; i < countResume; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                countResume--;
+                System.arraycopy(storage, i + 1, storage, i, countResume - i);
+                storage[countResume] = null;
+                return;
+            }
+        }
+    }
+
+    public Resume get(String uuid) {
+        for (int i = 0; i < countResume; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return storage[i];
+            }
+        }
+        return null;
+    }
+
+    public void clear() {
+        Arrays.fill(storage, 0, countResume, null);
+        countResume = 0;
     }
 }
